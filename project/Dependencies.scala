bd566bb9ac384e7df5154da2bdd3283bce2b5c07
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-09-23 10:09:53 +0100

    Update aws utils version

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -6,7 +6,7 @@ object Dependencies {
   lazy val authUtils = "uk.gov.nationalarchives" %% "tdr-auth-utils" % "0.0.25"
   lazy val awsCloudfront = "com.amazonaws" % "aws-java-sdk-cloudfront" % "1.12.47"
   lazy val awsLambda = "com.amazonaws" % "aws-lambda-java-core" % "1.2.1"
-  lazy val awsUtils = "uk.gov.nationalarchives.aws.utils" %% "tdr-aws-utils" % "0.1.19-SNAPSHOT"
+  lazy val awsUtils = "uk.gov.nationalarchives.aws.utils" %% "tdr-aws-utils" % "0.1.19"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.2.9"
   lazy val circeCore = "io.circe" %% "circe-core" % circeVersion
   lazy val circeParser = "io.circe" %% "circe-parser" % circeVersion
