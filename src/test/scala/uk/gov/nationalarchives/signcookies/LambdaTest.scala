83f8ea108e8bfacf2104f308567d87f5bd0733b4
MancunianSam <sam.palmer@nationalarchives.gov.uk>
2021-11-17 16:16:23 +0000

    Handle incoming Origin header.
    Some browsers (Edge) send the origin header as Origin which circe
    doesn't like. This will allow either Origin or origin. Hopefully we
    won't find an ancient MS browser that likes to send ORIGIN.

--- src/test/scala/uk/gov/nationalarchives/signcookies/LambdaTest.scala
+++ src/test/scala/uk/gov/nationalarchives/signcookies/LambdaTest.scala
@@ -66,4 +66,14 @@ class LambdaTest extends AnyFlatSpec with BeforeAndAfterEach {
 
     decode[LambdaResponse](byteArrayCaptor.getValue.map(_.toChar).mkString).toOption.get.statusCode should equal(200)
   }
+
+  "the lambda" should "does not error if the origin header has an upper case first letter" in {
+    val accessToken = createAccessToken(UUID.randomUUID().some)
+    val inputStream = getInputStream(accessToken, originKey = "Origin")
+    val byteArrayCaptor: ArgumentCaptor[Array[Byte]] = ArgumentCaptor.forClass(classOf[Array[Byte]])
+
+    new Lambda().handleRequest(inputStream, outputStream(byteArrayCaptor), null)
+
+    decode[LambdaResponse](byteArrayCaptor.getValue.map(_.toChar).mkString).toOption.get.statusCode should equal(200)
+  }
 }
