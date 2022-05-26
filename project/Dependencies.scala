d02fc8ddc4bf1e695e265fae1c1a577ee2c4485a
tna-digital-archiving-jenkins <digitalpreservation@nationalarchives.gov.uk>
2022-05-26 08:29:39 +0000

    Update aws-java-sdk-cloudfront to 1.12.228

--- project/Dependencies.scala
+++ project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   val pureConfigVersion = "0.17.1"
 
   lazy val authUtils = "uk.gov.nationalarchives" %% "tdr-auth-utils" % "0.0.55"
-  lazy val awsCloudfront = "com.amazonaws" % "aws-java-sdk-cloudfront" % "1.12.227"
+  lazy val awsCloudfront = "com.amazonaws" % "aws-java-sdk-cloudfront" % "1.12.228"
   lazy val awsLambda = "com.amazonaws" % "aws-lambda-java-core" % "1.2.1"
   lazy val awsUtils = "uk.gov.nationalarchives" %% "tdr-aws-utils" % "0.1.31"
   lazy val catsEffect = "org.typelevel" %% "cats-effect" % "3.3.12"
